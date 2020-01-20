abb50fd280ccd1db87cd45abf1f93a5122cc5fc6
Setherino <58416047+Setherino@users.noreply.github.com>
2020-01-20 15:36:22 -0700

    Taskpannel escape bug
    fixed bug with task pannel

--- Scripts/UI/HudController.gd
+++ Scripts/UI/HudController.gd
@@ -149,6 +149,9 @@ func hideInventory(): #hide inventory
 #when any key is pressed
 func _input(event):
 	if Input.is_action_just_pressed("inventory"): #if it was one of the inventory buttons
+		if has_node("Message"):
+			return
+		
 		if inventoryOpen: #if the inventory is already open
 			hideInventory()
 		else:
